0888809bd65f5092fbe5a5a8ee0c894c7878e795
anwaaralikhan <manwaaronline@gmail.com>
2019-01-12 15:58:02 -0500

    Spring Boot Project with (Docker) + (Spotify) Plugin

---
+++ docker-spotify/src/main/java/com/dc/spring/docker/spotify/DockerSpotifyApplication.java
@@ -0,0 +1,12 @@
+package com.dc.spring.docker.spotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpotifyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpotifyApplication.class, args);
+	}
+}
+
