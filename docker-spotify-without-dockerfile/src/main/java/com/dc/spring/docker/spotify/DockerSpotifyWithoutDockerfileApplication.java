31cdb3dc6bfc5e6db7ce96e0e90c49b837194ceb
anwaaralikhan <manwaaronline@gmail.com>
2019-01-12 18:33:39 -0500

    Docker Project with no DockerFile
    All Configurations must be defined in POM.xml

---
+++ docker-spotify-without-dockerfile/src/main/java/com/dc/spring/docker/spotify/DockerSpotifyWithoutDockerfileApplication.java
@@ -0,0 +1,14 @@
+package com.dc.spring.docker.spotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpotifyWithoutDockerfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpotifyWithoutDockerfileApplication.class, args);
+	}
+
+}
+
